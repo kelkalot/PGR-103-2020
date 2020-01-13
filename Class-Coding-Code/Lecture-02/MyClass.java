77738727d1bd7454417672a94a6d76b0b9adbad4
kelkalot <kelkalot@gmail.com>
2020-01-13 12:35:47 +0100

    class coding code lecture 02
    simple example for ide introduction

---
+++ Class-Coding-Code/Lecture-02/MyClass.java
@@ -0,0 +1,22 @@
+public class MyClass {
+
+
+    public static void main(String[] args) {
+
+        int x;
+        x =  1;
+
+
+        System.out.println(x);
+
+
+        byte b = Byte.MIN_VALUE;
+
+        System.out.println(b-1);
+
+    }
+
+
+
+
+}

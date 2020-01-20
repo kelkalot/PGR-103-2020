72c0f1908a1c5512803e7f53e320ddda1cb19364
kelkalot <kelkalot@gmail.com>
2020-01-20 13:23:09 +0100

    class coding lecture 03

---
+++ Class-Coding-Code/Lecture-03/MyTest.java
@@ -0,0 +1,26 @@
+public class MyTest {
+
+    public static void main(String[] args) {
+
+
+
+        for (int i=0;i<=5;i++){
+            System.out.println(i);
+
+        }
+
+        if(1==1){
+            int x = 1;
+        }
+
+        do{int y = 1;}
+        while(1==1);
+
+
+
+
+
+
+    }
+
+}
